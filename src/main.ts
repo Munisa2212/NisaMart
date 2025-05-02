d0438361bbc93eae1a462faf66873ea08f2b469d
Munisa <munisa@Munisas-MacBook-Pro.local>
2025-05-02 15:48:48 +0500

    changed

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
-    .setTitle('NisaMart')
+    .setTitle('Mart')
     .setDescription('The shop API description')
     .setVersion('1.0')
     .addSecurityRequirements('bearer', ['bearer'])
